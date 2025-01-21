4945e74e9b59636e6ecb7e35c053d1bd8cfc2095
badr <bernardoantoniod@gmail.com>
2025-01-21 20:29:05 -0300

    details and font

--- src/Components/UsPage/UsPage.jsx
+++ src/Components/UsPage/UsPage.jsx
@@ -52,7 +52,7 @@ function Us () {
             <section className={styles.us_middle_section_main_container}>
                 <section className={styles.us_middle_section_description_container}>
                     <h2 className={styles.us_subtitle}>Conoce nuestro equipo</h2>
-                    <p className={styles.us_paragraph}>Estamos conformados por profesionales de alto grado de experiencia en el mercado eléctrico del país, presentes en faenas de importantes empresas del rubro eléctrico, especialistas en el diagnóstico, mantención, resolución de problemas y soluciones integrales en el ámbito eléctrico, industrial y de manufactura. Nuestro servicio profesional y técnico está comprometido en la entrega de un servicio de calidad con altos estándares de calidad y seguridad. </p>
+                    <p className={styles.us_paragraph}>Estamos conformados por profesionales de alto grado de experiencia en el mercado eléctrico del país, presentes en faenas de importantes empresas del rubro eléctrico, especialistas en el diagnóstico, mantención, resolución de problemas y soluciones integrales en el ámbito eléctrico, industrial y de manufactura. Nuestro servicio profesional y técnico está comprometido en la entrega de un óptimo servicio con altos estándares de calidad y seguridad.</p>
                 </section>
                 <section className={styles.us_middle_section_cards_container}>
                     {
