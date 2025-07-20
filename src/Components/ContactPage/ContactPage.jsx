c50d277465b52621c7f5d066a4fa1cb68e2b68d6
badr <bernardoantoniod@gmail.com>
2025-07-20 14:05:27 -0400

    add conection with contact form backend, also change font type

--- src/Components/ContactPage/ContactPage.jsx
+++ src/Components/ContactPage/ContactPage.jsx
@@ -1,13 +1,9 @@
-import emailjs from "emailjs-com"
+import { useState } from "react";
 import styles from "./ContactPage.module.css"
 
 function ContactPage () {
 
-    function sendEmail(e) {
-        e.preventDefault();
-        emailjs.sendForm("service_gaq8idc", "template_x4pfd4h", e.target, "YcRp-roGG6YaNeCTe")
-        console.log("Enviado")
-        
+    const backToEmptyFormIputs = () => {
         document.getElementById('name').value = '';
         document.getElementById('lastName').value = '';
         document.getElementById('region').value = '';
@@ -15,11 +11,46 @@ function ContactPage () {
         document.getElementById('phone').value = '';
         document.getElementById('toWho').selectedIndex = 0;
         document.getElementById('message').value = '';
-        
-        window.scrollTo({ top: 0, behavior: 'smooth' });
-        alert("Mensaje enviado correctamente")
     }
 
+    const [data, setData] = useState({
+        name: "",
+        lastName: "",
+        region: "",
+        email: "",
+        phone: "",
+        toWho: "",
+        message: ""
+    })
+
+    const handleInputs = (e) => {
+        setData(...data, [e.target.name] = e.target.value)
+    }
+
+    async function sendEmail(e) {
+        e.preventDefault();
+        try {
+            const response = await fetch ("https//:hvtest.cl/contact.php", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(data)
+            })
+            const result = await response.json()
+            if (result.sucess) {
+                alert("Mensaje enviado")
+                backToEmptyFormIputs()
+                window.scrollTo({ top: 0, behavior: 'smooth' });
+            } else {
+                throw new Error(result)
+                alert("Error: " + result.error)
+            }
+        } catch (error) {
+            alert("Error al enviar el mensaje.")
+            backToEmptyFormIputs()
+        }
+    }
 
     return (
         <section className={styles.contact_main_section_container}>
@@ -32,23 +63,23 @@ function ContactPage () {
                 className={styles.contact_form}>
                 <section className={styles.contact_form_sections}>
                     <label for="name">Nombre(s) *</label>
-                    <input type="text" name="name" id="name" placeholder="Nombre" onSubmit="" required/>
+                    <input type="text" name="name" id="name" placeholder="Nombre" onSubmit="" required onChange={handleInputs}/>
                     <label for="lastName">Apellido(s) *</label>
-                    <input type="text" name="lastName" id="lastName" placeholder="Apellido" required/>
+                    <input type="text" name="lastName" id="lastName" placeholder="Apellido" required onChange={handleInputs}/>
                 </section>
                 <br />
                 <section className={styles.contact_form_sections}>
                     <label for="region">Región *</label>
-                    <input type="text" name="region" id="region" placeholder="Región" required/>
+                    <input type="text" name="region" id="region" placeholder="Región" required onChange={handleInputs}/>
                     <label for="email">Correo *</label>
-                    <input type="email" name="email" id="email" placeholder="Correo eléctronico" required/>
+                    <input type="email" name="email" id="email" placeholder="Correo eléctronico" required onChange={handleInputs}/>
                 </section>
                 <br />
                 <section className={styles.contact_form_sections}>
                     <label for="phone">Telefono *</label>
-                    <input type="tel" name="phone" id="phone" placeholder="Número telefonico" required/>
+                    <input type="tel" name="phone" id="phone" placeholder="Número telefonico" required onChange={handleInputs}/>
                     <label for="toWho">A quien va dirigido</label>
-                    <select name="toWho" id="toWho">
+                    <select name="toWho" id="toWho" onChange={handleInputs}>
                         <option value="general" selected>General</option>
                         <option value="pruebas">Pruebas eléctricas</option>
                         <option value="control">Control y protecciones</option>
